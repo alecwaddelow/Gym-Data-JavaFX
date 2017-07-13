b4efd7f044279087fb36e3c134619922f8411791
Marty Sease <martygpa@gmail.com>
2017-07-12 23:03:00 -0400

    Got empty gui working with fxml file.

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,6 +1,10 @@
 package gui;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -10,15 +14,14 @@ public class GUI extends Application {
 
 	Button button;
 	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Title");
-		this.button = new Button();
-		this.button.setText("Click Me And Do Nothing");
-		StackPane layout = new StackPane();
-		layout.getChildren().add(this.button);
-		Scene scene = new Scene(layout, 400, 400);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	public void start(Stage stage) throws IOException {
+		 Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+		    
+	        Scene scene = new Scene(root, 300, 275);
+	    
+	        stage.setTitle("FXML Welcome");
+	        stage.setScene(scene);
+	        stage.show();
 		
 	}
 
