1cdb03e9b223adc1a842deb1e3f8b6de6bffdc3f
Alec Waddelow <alec.waddelow@gmail.com>
2017-07-25 16:40:46 -0400

    Removed unused import

--- src/application/EntriesController.java
+++ src/application/EntriesController.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import Model.TripViewModel;
